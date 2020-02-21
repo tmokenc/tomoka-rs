85e485fbb6dd36d5dee9a5d213fc425ee87f2aa1
tmokenc <tmokenc@protonmail.com>
2020-02-21 21:25:06 +0100

    update to Rust 1.40 syntax

--- discord/src/commands/guild_master/logger/info.rs
+++ discord/src/commands/guild_master/logger/info.rs
@@ -6,31 +6,32 @@ use crate::commands::prelude::*;
 fn info(ctx: &mut Context, msg: &Message) -> CommandResult {
     let guild_id = match msg.guild_id {
         Some(id) => id,
-        None => return Ok(())
+        None => return Ok(()),
     };
-    
-    let config = crate::read_config();
-    let guild = config
-        .guilds
-        .get(&guild_id);
 
-    
-    msg.channel_id.send_message(ctx, |m| m.embed(|embed| {
-        embed.title("Logger information");
-        embed.color(config.color.information);
-        embed.timestamp(now());
-        
-        match guild {
-            Some(ref g) if g.logger.enable && g.logger.channel.is_some() => {
-                let c = g.logger.channel.unwrap();
-                embed.description(format!("The logger is on in <#{}>", c))
-            }
-            
-            _ => embed.description("The logger is disabled")
-        };
-        
-        embed
-    }))?;
+    let config = crate::read_config();
+    let log_channel = config
+        .guilds
+        .get(&guild_id)
+        .as_deref()
+        .filter(|v| v.logger.enable)
+        .and_then(|v| v.logger.channel);
+
+    msg.channel_id.send_message(ctx, |m| {
+        m.embed(|embed| {
+            embed.title("Logger information");
+            embed.color(config.color.information);
+            embed.timestamp(now());
+
+            match log_channel {
+                Some(channel) => embed.description(format!("The logger is on in <#{}>", channel)),
+
+                _ => embed.description("The logger is disabled"),
+            };
+
+            embed
+        })
+    })?;
 
     Ok(())
 }
