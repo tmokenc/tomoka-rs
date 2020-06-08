17652bf0723afc586c1827f8b14045365e272f7e
tmokenc <tmokenc@protonmail.com>
2020-06-09 04:05:33 +0900

    paginator

--- discord/src/traits/paginator.rs
+++ discord/src/traits/paginator.rs
@@ -10,91 +10,219 @@ use serenity::client::Context;
 use serenity::model::channel::{Message, ReactionType};
 use async_trait::async_trait;
 use crate::Result;
+use std::env;
+use core::str::FromStr;
+use magic::types::Void;
 
+#[derive(Debug, Clone)]
+pub struct PaginatorReactions {
+    reactions: [ReactionType; 5]
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PaginatorAction {
+    Page(usize),
+    First,
+    Previous,
+    Next,
+    Last,
+    Destroy,
+}
+
+impl FromStr for PaginatorAction {
+    type Err = Void;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let s = s.to_lowercase();
+        
+        let res = match s.trim() {
+            "first" => Self::First,
+            "previous" => Self::Previous,
+            "next" => Self::Next,
+            "last" => Self::Last,
+            s if s.starts_with("page") => Self::Page(s[4..].trim().parse()?),
+            _ => return Err(Void),
+        };
+        
+        Ok(res)
+    }
+
+}
+
+impl Default for PaginatorReactions {
+    #[rustfmt::skip]
+    fn default() -> Self {
+        let reactions = if let Ok(reactions) = env::var("PAGINATOR_REACTIONS") {
+            use ReactionType::Unicode;
+            let mut iter = reactions.split(',').map(ReactionType::try_from);
+                
+            [
+                iter.next().and_then(|v| v.ok()).unwrap_or_else(|| Unicode(String::from("⏮️"))),
+                iter.next().and_then(|v| v.ok()).unwrap_or_else(|| Unicode(String::from("◀️"))),
+                iter.next().and_then(|v| v.ok()).unwrap_or_else(|| Unicode(String::from("▶️"))),
+                iter.next().and_then(|v| v.ok()).unwrap_or_else(|| Unicode(String::from("⏭️"))),
+                iter.next().and_then(|v| v.ok()).unwrap_or_else(|| Unicode(String::from("❌"))),
+            ]
+        } else {
+            [
+                ReactionType::Unicode(String::from("⏮️")),
+                ReactionType::Unicode(String::from("◀️")),
+                ReactionType::Unicode(String::from("▶️")),
+                ReactionType::Unicode(String::from("⏭️")),
+                ReactionType::Unicode(String::from("❌")),
+            ]
+        };
+        
+        Self { reactions }
+    }
+}
+
+impl PaginatorReactions {
+    #[inline]
+    pub fn iter(&self) -> impl Iterator<Item=&ReactionType> {
+        self.reactions.iter()
+    }
+    
+    #[allow(dead_code)]
+    pub fn change(&mut self, kind: PaginatorAction, reaction: ReactionType) {
+        use PaginatorAction::*;
+        
+        let current = match kind {
+            First => &mut self.reactions[0],
+            Previous => &mut self.reactions[1],
+            Next => &mut self.reactions[2],
+            Last => &mut self.reactions[3],
+            Destroy => &mut self.reactions[4],
+            _ => return,
+        };
+        
+        *current = reaction;
+    }
+    
+    #[inline]
+    pub fn kind(&self, reaction: &ReactionType) -> Option<PaginatorAction> {
+        use PaginatorAction::*;
+        
+        self.iter().position(|ref v| v == &reaction).and_then(|v| match v {
+            0 => Some(First),
+            1 => Some(Previous),
+            2 => Some(Next),
+            3 => Some(Last),
+            4 => Some(Destroy),
+            _ => None
+        })
+    }
+}
 
 #[async_trait]
 pub trait Paginator {
     /// Notice that the page start at 1
     fn append_page_data<'a>(&self, page: usize, embed: &'a mut CreateEmbed) -> &'a mut CreateEmbed;
+    
+    #[inline]
+    fn default_page(&self) -> usize {
+        1
+    }
+    
+    #[inline]
     fn total_pages(&self) -> Option<usize> {
         None
     }
     
-    async fn pagination<C: Borrow<Context> + Send>(&self, ctx: C, msg: &Message) -> Result<()> {
-        const REACTIONS: &[&str] = &["◀️", "▶️", "❌"];
-        const WAIT_TIME: Duration = Duration::from_secs(30);
+    #[inline]
+    fn timeout(&self) -> Duration {
+        Duration::from_secs(30)
+    }
     
+    #[inline]
+    fn reactions(&self) -> PaginatorReactions {
+        Default::default()
+    }
+    
+    async fn pagination<C: Borrow<Context> + Send>(&self, ctx: C, msg: &Message) -> Result<()> {
         let ctx = ctx.borrow();
         let total = self.total_pages();
-        let mut current_page = 1;
+        let wait_time = self.timeout();
+        let reactions = self.reactions();
+        let mut current_page = self.default_page();
         let mut mess = msg
             .channel_id
             .send_message(ctx, |message| {
-                let reactions = REACTIONS
-                    .iter()
-                    .map(|&s| ReactionType::try_from(s).unwrap());
-    
-                message.reactions(reactions);
+                message.reactions(reactions.iter().cloned());
                 message.embed(|embed| self.append_page_data(current_page, embed));
                 message
             })
             .await?;
     
-        let mut collector = mess
-            .await_reactions(&ctx)
+        let react_collector = mess
+            .await_reactions(ctx)
             .author_id(msg.author.id)
-            .filter(|reaction| {
-                matches!(&reaction.emoji, ReactionType::Unicode(s) if REACTIONS.contains(&s.as_str()))
-            })
-            .await;
-    
-        while let Ok(Some(reaction)) = timeout(WAIT_TIME, collector.next()).await {
-            let reaction = reaction.as_inner_ref();
-    
-            let http = Arc::clone(&ctx.http);
-            let react = reaction.to_owned();
-            tokio::spawn(async move {
-                react.delete(http).await.ok();
+            .await
+            .filter_map(|reaction| {
+                let reaction = reaction.as_inner_ref().to_owned();
+                let result = reactions.kind(&reaction.emoji);
+                
+                async move {
+                    if result.is_some() {
+                        let http = Arc::clone(&ctx.http);
+                        tokio::spawn(async move {
+                            reaction.delete(http).await.ok();
+                        });
+                    }
+                
+                    result
+                }
             });
+            
+        let msg_collector = msg
+            .channel_id
+            .await_replies(ctx)
+            .author_id(msg.author.id)
+            .await
+            .filter_map(|v| async move {
+                v.content.parse::<PaginatorAction>().ok()
+            });
+            
+        let stream = futures::stream::select(react_collector, msg_collector);
+        futures::pin_mut!(stream);
     
-            let emoji = match &reaction.emoji {
-                ReactionType::Unicode(s) => s,
-                _ => continue,
-            };
-    
-            match emoji.as_str() {
-                "◀️" => {
-                    if current_page == 1 {
-                        continue;
-                    }
-    
-                    current_page -= 1;
+        while let Ok(Some(action)) = timeout(wait_time, stream.next()).await {
+            match action {
+                PaginatorAction::First if current_page > 1 => current_page = 1,
+                
+                PaginatorAction::Previous if current_page > 1 => current_page -= 1,
+                
+                PaginatorAction::Next => match total {
+                    Some(max) if current_page < max => current_page += 1,
+                    _ => continue,
                 }
-    
-                "▶️" => {
-                    if matches!(total, Some(max) if current_page >= max) {
-                        continue;
-                    }
-    
-                    current_page += 1;
+                
+                PaginatorAction::Last => match total {
+                    Some(max) => current_page = max,
+                    None => continue
                 }
-    
-                "❌" => {
+                
+                PaginatorAction::Page(page) if current_page != page => match total {
+                    Some(max) if page <= max => current_page = page,
+                    _ => current_page = page,
+                }
+                
+                PaginatorAction::Destroy => {
                     mess.delete(ctx).await?;
                     return Ok(());
                 }
+                
                 _ => continue,
             }
     
             mess.edit(ctx, |m| m.embed(|e| self.append_page_data(current_page, e))).await?;
         }
     
-        drop(collector);
+        drop(stream);
     
-        let futs = REACTIONS
-            .into_iter()
-            .map(|&s| ReactionType::try_from(s).unwrap())
-            .map(|s| msg.channel_id.delete_reaction(&ctx, mess.id.0, None, s));
+        let futs = reactions
+            .iter()
+            .cloned()
+            .map(|s| msg.channel_id.delete_reaction(ctx, mess.id.0, None, s));
     
         futures::future::join_all(futs).await;
         Ok(())
@@ -103,6 +231,7 @@ pub trait Paginator {
 
 impl<E: Embedable> Paginator for Vec<E> {
     fn append_page_data<'a>(&self, page: usize, embed: &'a mut CreateEmbed) -> &'a mut CreateEmbed {
+        embed.footer(|f| f.text(format!("{} / {}", page, self.len())));
         match self.get(page - 1) {
             Some(data) => data.append_to(embed),
             None => embed.description("This page does not exist")
