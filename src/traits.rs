5cc4ad575883bc5a7794cad69a086978b7111b18
tmokenc <tmokenc@protonmail.com>
2020-08-11 03:11:16 +0900

    remove the traits as it has moved into a seperated module

--- src/traits.rs
+++ src/traits.rs
@@ -1,11 +1,3 @@
-// pub mod embedable;
-// pub mod paginator;
-// pub mod channelext;
-//
-// pub use embedable::Embedable;
-// pub use paginator::{Paginator, PaginatorOption};
-// pub use channelext::ChannelExt;
-
 pub use tomo_serenity_ext::*;
 
 use crate::types::Ref;
